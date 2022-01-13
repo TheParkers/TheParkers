c52de09cceadb49ee560a887da279d6fdc2c9ad3
navjotsandhu49 <navjotsandhu49@gmail.com>
2022-01-13 12:21:01 -0500

    PD-31 #time 6h #comment Made connection with mysql-connector

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'sample',
+        'USER': 'root',
+        'PASSWORD': 'sqlpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+  
 
 
 # Password validation
