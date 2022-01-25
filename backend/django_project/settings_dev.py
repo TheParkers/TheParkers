7554030c1b0252233d3a93fa62c6d005970be8cb
subhambiswajit <digu35@gmail.com>
2022-01-24 21:46:33 -0500

    PD-53 Adding coverage for django tests

--- backend/django_project/settings_dev.py
+++ backend/django_project/settings_dev.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--cover-erase',
+    # '--cover-package=MY_APP',
+]
