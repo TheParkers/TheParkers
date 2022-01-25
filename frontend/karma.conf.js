a6675f81018cfb3560f8e55a2d9be03111c7274c
subhambiswajit <digu35@gmail.com>
2022-01-25 03:20:19 -0500

    adding karma docker launcher

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-docker-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
