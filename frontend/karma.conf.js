fa8d5b643801d5719c29bc36de004fe2c6f5133b
subhambiswajit <digu35@gmail.com>
2022-01-25 02:24:30 -0500

    Updating karma condif and docker

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -60,6 +60,14 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox:{
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox"
+        ]
+      }
+    }
   });
 };
