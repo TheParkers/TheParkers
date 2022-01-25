98030646b313aeeca90157219939907e25e149c4
subhambiswajit <digu35@gmail.com>
2022-01-24 22:03:41 -0500

    Adding coverage to django project

--- backend/django_project/settings_dev.py
+++ backend/django_project/settings_dev.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -127,10 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-    '--cover-erase',
-    # '--cover-package=MY_APP',
-]
