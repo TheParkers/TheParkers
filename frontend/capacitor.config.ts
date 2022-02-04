b5840eabf0cdb1abc1562cd09c872f935d696179
jaydeepsingh25 <jaydeepsingh_25@yahoo.com>
2022-02-03 20:06:12 -0500

    PD-28 #time 5d #comment home page design

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,8 +3,19 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'frontend',
-  webDir: 'www',
+  webDir: 'dist/frontend',
   bundledWebRuntime: false
+  // cordova: {
+  //   preferences: {
+  //     ScrollEnabled: 'false',
+  //     BackupWebStorage: 'none',
+  //     SplashMaintainAspectRatio: 'true',
+  //     FadeSplashScreenDuration: '300',
+  //     SplashShowOnlyFirstTime: 'false',
+  //     SplashScreen: 'screen',
+  //     SplashScreenDelay: '3000'
+  //   }
+  // }
 };
 
 export default config;
