d1f29532dd385af902316b09cc3f8426ddc911bd
Samarth Jain <samarth29@gmail.com>
2022-02-07 09:36:59 -0500

    PD-73 #time 2h #comment Complete code coverage missed in last sprint (#37)
    * PD-58 #time 2d #comment creating dummy model and adding UT for the dummy model
    
    * PD-73 #time 2h #comment Complete code coverage missed in last sprint
    
    * fixing unit test
    
    * revert changes
    
    * Fixing unit tests
    
    * PD-73 #time 3h Adding more tests covering scenarios.
    
    Co-authored-by: subhambiswajit <digu35@gmail.com>

--- backend/apps/users/test/test_users.py
+++ backend/apps/users/test/test_users.py
@@ -1,22 +1,22 @@
+from re import S
 from unittest import mock
 from unittest.mock import patch
-from django.test import TestCase
-from django.urls import reverse
 from django.http import JsonResponse
 
 from ..models import User
 from ..serializers import UserSerializer
-from rest_framework.test import APITestCase, RequestsClient
+from rest_framework.test import APITestCase, APIRequestFactory
 from rest_framework import status
 
 class TestUserModel(APITestCase):
+    client = APIRequestFactory()
     @patch('apps.users.models.User.objects')
     def setUp(self, mockUser):
         sampleuser_1 = User.objects.create(userName="TestUser2", userType="root")
         sampleuser_2 = User.objects.create(userName="TestUser3", userType="guest")    
 
     def test_get(self):
-        response = self.client.get('http://testserver/users/')
+        response = self.client.get('/users/')
 
         # assert response code.
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -28,11 +28,39 @@ class TestUserModel(APITestCase):
         if serializer.is_valid():
             users_json = JsonResponse(serializer.data, safe=False)
             self.assertJSONEqual(users_json, response.content)
-            
-    def test_get_one(self):
+
+    @patch('apps.users.models.User.objects')       
+    def test_get_one(self, mockUser):
         testuser_3 = User.objects.create(userName="TestUser2", userType="root")
-        response = self.client.get('http://testserver/users/1')
-        data = response.json()
-        self.assertEqual(data.get('userName'), testuser_3.userName)
-        self.assertEqual(data.get('userType'), testuser_3.userType)
+        response = self.client.get('/users/1/')
+        single_user = User.objects.get(pk=1)
+        serializer = UserSerializer(data=single_user, many=False)
+        if serializer.is_valid():
+            user_json = JsonResponse(serializer.data, safe=False)
+            self.assertJSONEqual(user_json, response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post(self):
+        resp = self.client.post("/users/", {'userName': "PutUser_1", "userType":"admin"}, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+    
+    def test_post_bad_request(self):
+        resp = self.client.post("/users/", {'faultyparam': "PutUser_1", "faultyparam":"admin"}, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_put(self):
+       resp = self.client.put("/users/5/", {'userName': "PutUser_5", "userType":"admin"}, format='json')
+       self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    
+    def test_put_bad_request(self):
+       resp = self.client.put("/users/5/", {'faultyparam': "PutUser_5", "faultyparam":"admin"}, format='json')
+       self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete(self):
+        testuser_3 = User.objects.create(userName="TestUser2", userType="root")
+        resp = self.client.delete('/users/1/')
+        self.assertEqual(resp.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_get_nonexistent_user(self):
+        resp = self.client.delete('/users/123456/')
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+
+@api_view(['GET', 'POST'])
 def users_list(request):
     if request.method == 'GET':
         usersList = User.objects.all()
@@ -11,34 +15,34 @@ def users_list(request):
         return JsonResponse(userSerializer.data, safe=False)
 
     if request.method == 'POST':
-        data = JSONParser.parse(request.data)
-        serializer = UserSerializer(data=data)
-
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['PUT', 'GET', 'DELETE'])
 def user_mod(request, pk):
 
-    try:
-        user = User.objects.get(pk=pk)
-    except Exception as e:
-        return HttpResponse(status=404)
-    
+    if request.method == 'GET' or request.method == 'DELETE':
+        try:
+            user = User.objects.get(pk=pk)
+        except Exception as e:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        user_data_ser = UserSerializer(data=request.data)
+        if user_data_ser.is_valid():
+            user_data_ser.save()
+            return JsonResponse(user_data_ser.data, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(user_data_ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
     if request.method == 'GET':
         userSer = UserSerializer(user)
-        return JsonResponse(userSer.data)
+        return JsonResponse(userSer.data, status=status.HTTP_200_OK)
 
     if request.method == 'DELETE':
         user.delete()
-
-    if request.method == 'PUT':
-        user_data = JSONParser.parse(request.data)
-        user_data_ser = UserSerializer(data=user_data)
-        if user_data_ser.is_valid():
-            user_data_ser.save()
-            return JsonResponse(user_data_ser.data, status=201)
-        else:
-            return JsonResponse(user_data_ser.errors, status=400)
+        return HttpResponse(status=status.HTTP_202_ACCEPTED)
