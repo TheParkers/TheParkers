67170a4430db2b6d783992820337bf82cc9337fd
subhambiswajit <digu35@gmail.com>
2022-02-09 17:13:31 -0500

    removing django default auth user

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+class User(models.Model):
     tpk_firebaseid = models.CharField(max_length=100)
     tpk_photoUrl = models.CharField(max_length=500, default="NA")
 
