97dc026b5d7fdcb3cb1dd276bf4702078f1cc086
subhambiswajit <digu35@gmail.com>
2022-02-10 06:36:37 -0500

    adding minor fixes

--- backend/apps/parkersauth/permissions/IsUserLoggedIn.py
+++ backend/apps/parkersauth/permissions/IsUserLoggedIn.py
@@ -1,7 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 class IsUserLoggedIn(BasePermission):
-    message = 'Only admin or teacher can edit student detail.'
     def has_permission(self, request, view):
         is_loggedIn = bool(request.user)
 
