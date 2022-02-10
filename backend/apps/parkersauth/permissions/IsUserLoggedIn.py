e36479056d7e76f80aeb7a9d501f1cc4c46d808f
subhambiswajit <digu35@gmail.com>
2022-02-10 07:35:52 -0500

    removing redundant print statement

--- backend/apps/parkersauth/permissions/IsUserLoggedIn.py
+++ backend/apps/parkersauth/permissions/IsUserLoggedIn.py
@@ -2,7 +2,6 @@ from rest_framework.permissions import BasePermission
 
 class IsUserLoggedIn(BasePermission):
     def has_permission(self, request, view):
-        print(request.user.is_anonymous)
         if str(request.user) == 'AnonymousUser':
             return False
         return True
