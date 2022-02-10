9985318226cd518e11f73a9f3037c801c79e9a1e
subhambiswajit <digu35@gmail.com>
2022-02-10 07:30:45 -0500

    fixing unit tests

--- backend/apps/parkersauth/permissions/IsUserLoggedIn.py
+++ backend/apps/parkersauth/permissions/IsUserLoggedIn.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsUserLoggedIn(BasePermission):
     def has_permission(self, request, view):
+        print(request.user.is_anonymous)
         if str(request.user) == 'AnonymousUser':
             return False
         return True
