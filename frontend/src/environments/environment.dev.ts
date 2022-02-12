986ed8dcabfd2c30ff9f1c3d5d960c7b253f3c93
subhambiswajit <digu35@gmail.com>
2022-02-12 01:57:38 -0500

    updating signin call

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -12,6 +12,11 @@ export const environment = {
     messagingSenderId: "111997111938",
     appId: "1:111997111938:web:39e0c8e1909756dcde6e9b",
     measurementId: "G-S566XNQGE2"
+  },
+  apiServer: 'http://localhost:8000',
+  apiUrls: {
+    registerUser: '/users/register/',
+    loginUser: '/signin/'
   }
 };
 
