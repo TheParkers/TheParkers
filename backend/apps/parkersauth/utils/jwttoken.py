e1943e653a4a7c5df6073aaa5d0f374402568612
subhambiswajit <digu35@gmail.com>
2022-02-16 06:52:22 -0500

    adding backend lint

--- backend/apps/parkersauth/utils/jwttoken.py
+++ backend/apps/parkersauth/utils/jwttoken.py
@@ -1,7 +1,12 @@
-import datetime
+'''
+Utility for jwt token related functionalities
+'''
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.conf import settings
+
 
 def generate_access_token(user):
+    '''
+        Utility: takes a user object and returns a JWT token.
+    '''
     access_token = RefreshToken.for_user(user)
     return access_token
--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,14 +1,24 @@
+'''
+    Serializer: user app
+'''
 from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    '''
+    Serializer: Generic User
+    '''
     class Meta:
         model = User
         fields = ['tpk_firebaseid']
         app_label = 'User'
 
 class UserResponseSerializer(serializers.ModelSerializer):
+    '''
+    Serializer: User for success response
+    '''
     class Meta:
         model = User
         fields = ['tpk_name', 'tpk_email', 'tpk_photoUrl']
         app_label = 'User'
+        
