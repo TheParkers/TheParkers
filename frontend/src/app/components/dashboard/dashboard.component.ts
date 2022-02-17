d4c7216ec890b1e2ec0f06fb5f1c41e45f7a19ce
subhambiswajit <digu35@gmail.com>
2022-02-17 07:26:36 -0500

    updating dashboard UI

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit(): void {
     console.log('dashboard')
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      cssClass: 'my-custom-class',
+      translucent: false,
+      buttons: [{
+        text: 'Bookings',
+        role: 'bookings',
+        icon: 'cart-outline',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('Bookings clicked');
+        }
+      }, {
+        text: 'Rentals',
+        icon: 'car-sport-outline',
+        data: 10,
+        handler: () => {
+          console.log('Rentals clicked');
+        }
+      }, {
+        text: 'Profile',
+        icon: 'person-circle-outline',
+        data: 'Data value',
+        handler: () => {
+          console.log('Profile clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
   }
 
 }
