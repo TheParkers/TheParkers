f5df1f79de6431304a5933fad6bb60b1d8cc7a58
Akshay Sharma <a483shar@uwaterloo.ca>
2022-02-20 10:27:13 +0530

    Removing pre-loader component reference

--- frontend/src/app/components/index.ts
+++ frontend/src/app/components/index.ts
@@ -1,4 +1,4 @@
 export { SearchParkingComponent } from "./search-parking/search-parking.component";
 export { HomeComponent } from './home/home.component';
 export { DashboardComponent } from './dashboard/dashboard.component';
-export { PreloaderComponent } from './pre-loader/pre-loader.component';
+
