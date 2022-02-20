9bab4aad42313ccac12f088e1ce0c6052c77971b
Akshay Sharma <a483shar@uwaterloo.ca>
2022-02-20 09:56:33 +0530

    Preloader service

--- frontend/src/app/services/index.ts
+++ frontend/src/app/services/index.ts
@@ -2,4 +2,6 @@ export { FirebaseService } from "./firebase/firebase.service";
 export { GuardService } from "./guard/guard.service";
 export { AuthService } from "./auth/auth.service";
 export { LocalStorageService } from "./local-storage/local-storage.service";
+export { PreLoaderService } from "./pre-loader/pre-loader.service";
 export { HttpInterceptorService } from './http-interceptor/http-interceptor.service';
+
