55d56eecef2955d350fd77027aa503ed28cfea7b
Samarth Jain <samarth29@gmail.com>
2022-02-22 11:17:31 -0500

    PD-111 #time 8h #comment parking space model and basic get api with testcase.

---
+++ backend/apps/parkingspace/models.py
@@ -0,0 +1,25 @@
+from sys import maxsize
+from django.db import models
+from django.utils import timezone
+
+class ParkingSpaceFeatures(models.Model):
+    has_car_charging = models.BooleanField(default=False)
+    has_car_wash = models.BooleanField(default=False)
+    has_indoor_parking = models.BooleanField(default=False)
+
+class ParkingSpace(models.Model):
+    parking_area = models.FloatField()
+    #TODO need to link userID who has valid permissions for registering their parking space.
+    has_features = models.BooleanField(default=False)
+    vehicle_capacity = models.IntegerField()
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    area_code = models.CharField(max_length=20) #create index to search based on area_code faster.
+    # our pricing model will use this code to figure out the pricing to select.
+    lat = models.FloatField()
+    long = models.FloatField()
+    parking_features = models.OneToOneField(ParkingSpaceFeatures, blank=True, null=True, on_delete=models.CASCADE, related_name="parking_features")
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_booked = models.DateTimeField()
+    is_booked = models.BooleanField(default=False)
---
+++ backend/apps/parkingspace/tests/test_parkingspace.py
@@ -0,0 +1,26 @@
+from unittest.mock import patch
+from django.http import JsonResponse
+
+from apps.parkingspace.models import ParkingSpace, ParkingSpaceFeatures
+from apps.parkingspace.serializers import ParkingSpaceSerializer
+from apps.parkingspace.views import *
+from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework import status
+from django.utils import timezone
+
+class TestParkingSpaceModel(APITestCase):
+    @patch('apps.users.models.User.objects.get')
+    def setUp(self, mockParkingSpace):
+        parkingspacefeature_1 = ParkingSpaceFeatures.objects.create(has_car_charging = True, has_car_wash = False, has_indoor_parking = False)
+        mockParkingSpace.return_value = ParkingSpace.objects.create(parking_area = 100, has_features = True, vehicle_capacity = 1, address = "200 University Avenue West",
+                                        city = "Waterloo", country = "Canada", area_code = "N2L6P1", lat = 20.0001, long = 31.1234, 
+                                        parking_features = parkingspacefeature_1, created_on = timezone.now(), last_booked = timezone.now(), is_booked = False)
+
+    def test_get(self):
+        response = self.client.get('/parking')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        parkingspaces = ParkingSpace.objects.all()
+        serializer = ParkingSpaceSerializer(parkingspaces, many=True)
+        #note that the get returns queryset, so we are using many=True. 
+        ps_json_resp = JsonResponse(serializer.data, safe=False)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), str(ps_json_resp.content, encoding='utf8'))
