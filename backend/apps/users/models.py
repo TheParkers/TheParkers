b7d48b7637a7551c87ee8a2414800e39ba0731ae
yyuzhako <yyuzhako@uwaterloo.ca>
2022-02-22 17:52:05 -0700

    reformat code

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -4,7 +4,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-class User(models.Model):
+class User(AbstractBaseUser, PermissionsMixin):
     '''
     Model: User
     tpk_firebaseid: maxlenth=100
