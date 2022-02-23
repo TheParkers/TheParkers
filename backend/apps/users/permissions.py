78ff9d84751cc8dfa9453fa2542116f495ad3454
yyuzhako <yyuzhako@uwaterloo.ca>
2022-02-22 17:52:05 -0700

    reformat code

--- backend/apps/users/permissions.py
+++ backend/apps/users/permissions.py
@@ -1,40 +1,38 @@
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
-def assignPermissionsToRoles():
+def assign_permissions_to_roles():
     '''
         Create user roles
     '''
-    superuser_role, created = Group.objects.get_or_create(name='SUPERUSER')
-    tenant_role, created = Group.objects.get_or_create(name='TENANT')
-    landlord_role, created = Group.objects.get_or_create(name='LANDLORD')
-    viewer_role, created = Group.objects.get_or_create(name='VIEWER')
+    Group.objects.get_or_create(name='SUPERUSER')
+    Group.objects.get_or_create(name='TENANT')
+    Group.objects.get_or_create(name='LANDLORD')
+    Group.objects.get_or_create(name='VIEWER')
     
-    '''
-        Assign Permissions to roles
-    '''
+    #Assign Permissions to roles
     # all permissions to superuser
-    SUPERUSER = Group.objects.get(name='SUPERUSER')
+    super_user = Group.objects.get(name='SUPERUSER')
     superuser_permissions = list(Permission.objects.all())
     try:
-        SUPERUSER.permissions.add(*superuser_permissions)
+        super_user.permissions.add(*superuser_permissions)
     except:
         pass
 
-    TENANT = Group.objects.get(name='TENANT')
+    tenant = Group.objects.get(name='TENANT')
     tenant_permissions = [] #TBD
-    TENANT.permissions.set(tenant_permissions)
+    tenant.permissions.set(tenant_permissions)
 
-    LANDLORD = Group.objects.get(name='LANDLORD')
+    landlord = Group.objects.get(name='LANDLORD')
     landlord_permissions = [] #TBD
-    LANDLORD.permissions.set(landlord_permissions)
+    landlord.permissions.set(landlord_permissions)
 
 
     # all view permissions to viewers
-    VIEWER = Group.objects.get(name='VIEWER')
+    viewer = Group.objects.get(name='VIEWER')
     for content_type in ContentType.objects.all():
-        viewer_permissions = list(Permission.objects.filter(name = "Can view %s" % content_type.name))
+        viewer_permissions = list(Permission.objects.filter(name="Can view %s" % content_type.name))
         try:
-            VIEWER.permissions.add(*viewer_permissions)
+            viewer.permissions.add(*viewer_permissions)
         except:
             pass
