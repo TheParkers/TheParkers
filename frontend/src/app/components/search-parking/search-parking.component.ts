c86c3a8f2c0b263f16d44ce79cd9bf6505aaaccf
jaydeepsingh25 <jaydeepsingh_25@yahoo.com>
2022-02-24 13:39:03 -0500

    PD-109 #time 2d #comment parking page design

--- frontend/src/app/components/search-parking/search-parking.component.ts
+++ frontend/src/app/components/search-parking/search-parking.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-parking',
@@ -8,10 +9,13 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class SearchParkingComponent {
 
-  constructor() { }
+  constructor(private router:Router) { }
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl(),
   });
-
+  
+  navigateToParkingList(){
+    this.router.navigateByUrl('/parkings');
+  }
 }
