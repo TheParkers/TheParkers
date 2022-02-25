211462d86bd5b01787b4c3855e1e6603c64ded18
subhambiswajit <sbbal@uwaterloo.ca>
2022-02-25 04:17:05 -0500

    updating dashboard ui

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
-import { FirebaseService } from 'src/app/services';
+import { AuthService, FirebaseService } from 'src/app/services';
 import { UserService } from 'src/app/services/user/user.service';
 import { environment } from 'src/environments/environment.dev';
 
@@ -9,14 +9,29 @@ import { environment } from 'src/environments/environment.dev';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  dashboard_user: any;
   constructor(private actionSheetCtrl: ActionSheetController, 
-              private firebaseService: FirebaseService
-              ) {}
+              private firebaseService: FirebaseService,
+              private parkerAuth: AuthService
+              ) {
+                
+              }
+  async ngOnInit() {
+    this.parkerAuth.getSignedInUser().subscribe({
+      next: response => {
+          this.dashboard_user = response
+      },
+      error: (error) => {
+        console.log('get y=user failed in dashboard', error)
+      }
+    })
+    
+  }
 
    /* istanbul ignore next */
   public async presentActionSheet() {
+    console.log('inside action sheet',  this.firebaseService.authUser)
     const actionSheet = await this.actionSheetCtrl.create(environment.actionSheetConfig(this.firebaseService));
     console.log(actionSheet)
     await actionSheet.present();
