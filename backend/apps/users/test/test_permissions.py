97e5b8a6365ab58c0f77a99fb263d2e4636c3526
Yakov Yuzhakov <yyuzhako@uwaterloo.ca>
2022-03-02 22:55:53 -0700

    update test cases

--- backend/apps/users/test/test_permissions.py
+++ backend/apps/users/test/test_permissions.py
@@ -93,3 +93,42 @@ class TestPermissions(APITestCase):
         perm_json_resp = JsonResponse(roles_seria.data, safe=False)
         self.assertJSONEqual(str(response.content, encoding='utf8'),
                             str(perm_json_resp.content, encoding='utf8'))
+
+    @patch('apps.parkersauth.permissions.isuserloggedin.IsUserLoggedIn.has_permission')     
+    def test_assign_role(self, mockPerm):
+        response = self.client.put('/users/testid/roles', {"tpk_assign_role": "SUPERUSER"}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        
+        #compare roles
+        response = self.client.get('/users/testid/roles')
+        request_user = User.objects.get(tpk_email = 'test_email@test.com')
+        user_role = request_user.groups.all()
+        roles_seria = RoleSerializer(user_role,many=True)
+        perm_json_resp = JsonResponse(roles_seria.data, safe=False)
+        self.assertJSONEqual(str(response.content, encoding='utf8'),
+                            str(perm_json_resp.content, encoding='utf8'))
+
+        #compare permissions
+        response = self.client.get('/users/testid/permissions')
+        old_user_role = Group.objects.get(name='VIEWER')
+        new_user_role = Group.objects.get(name='SUPERUSER')
+        old_permissions_listed = old_user_role.permissions.all()
+        new_permissions_listed = new_user_role.permissions.all()
+        permissions_seria = PermissionSerializer(old_permissions_listed.union(new_permissions_listed), many=True)
+        perm_json_resp = JsonResponse(permissions_seria.data, safe=False)
+        self.assertEqual(len(str(response.content, encoding='utf8')),
+                            len(str(perm_json_resp.content, encoding='utf8')))
+
+    @patch('apps.parkersauth.permissions.isuserloggedin.IsUserLoggedIn.has_permission')     
+    def test_remove_role(self, mockPerm):
+        response = self.client.delete('/users/testid/roles', {"tpk_remove_role": "SUPERUSER"}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        
+        #compare roles
+        response = self.client.get('/users/testid/roles')
+        request_user = User.objects.get(tpk_email = 'test_email@test.com')
+        user_role = request_user.groups.all()
+        roles_seria = RoleSerializer(user_role,many=True)
+        perm_json_resp = JsonResponse(roles_seria.data, safe=False)
+        self.assertJSONEqual(str(response.content, encoding='utf8'),
+                            str(perm_json_resp.content, encoding='utf8'))
