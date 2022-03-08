e21033cae8be0b19090b868def53da27fbd1a337
subhambiswajit <sbbal@uwaterloo.ca>
2022-03-08 02:03:07 -0500

    simplying auth flows

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -13,20 +13,17 @@ export class DashboardComponent {
   dashboard_user: any;
   constructor(private actionSheetCtrl: ActionSheetController, 
               private firebaseService: FirebaseService,
-              private parkerAuth: AuthService,
-              private routerService: Router,
-              private preloaderService:PreLoaderService
+              private router: Router
               ) 
               {
-                this.preloaderService.presentLoader()
-                this.parkerAuth.getSignedInUser().subscribe({
-                  next: response => {
-                    this.dashboard_user = response.user
+                this.firebaseService.getAuthUser().subscribe({
+                  next: response  => {
+                      this.dashboard_user = response.user
                   },
-                  error: (error) => {
-                    this.preloaderService.dismissLoader()
-                    console.log('get user failed in dashboard', error)
-                    this.routerService.navigate(['/home'])
+                  error: error => {
+                    this.firebaseService.clearAuthStates()
+                    console.log("Error in get signed user, dashboard", error)
+                    this.router.navigate(['/home'])
                   }
                 })
               }
