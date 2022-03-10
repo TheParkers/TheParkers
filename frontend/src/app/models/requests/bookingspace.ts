a42e9cc2290bb99875b83292af6a7bc493c9adc7
Akshay Sharma <akshay.k.sharma986@gmail.com>
2022-03-11 04:17:36 +0530

    Correcting Namespace

--- frontend/src/app/models/requests/bookingspace.ts
+++ frontend/src/app/models/requests/bookingspace.ts
@@ -1,7 +1,7 @@
-export interface bookingspaceDetails {
+export interface bookingspace {
     tpk_parkingspace: number;
-    tpk_book_start_datetime : Date
-    tpk_book_end_datetime: Date
-    tpk_firebaseid: string
+    tpk_book_start_datetime : Date;
+    tpk_book_end_datetime: Date;
+    tpk_firebaseid: string;
 
 }
