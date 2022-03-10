3227de057b4ead805d0c32c36035c03e8de43d18
Akshay Sharma <akshay.k.sharma986@gmail.com>
2022-03-10 22:16:11 +0530

    Adding booking space component

---
+++ frontend/src/app/components/booking/booking.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { BookingService } from 'src/app/services/booking';
+
+@Component({
+  selector: 'app-booking',
+  templateUrl: './booking.component.html',
+  styleUrls: ['./booking.component.scss']
+})
+export class BookingComponent {
+
+  constructor(public bookings: BookingService ) {
+
+
+  }
+ 
+  booking() {
+    this.bookings.booking().then( () => {
+        console.log('booking success from app')
+    })
+  }
+}
