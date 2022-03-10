7786ab7a5f8bf385f7d61d84c7ddb0012751d2b7
Yakov Yuzhakov <yyuzhako@uwaterloo.ca>
2022-03-09 20:07:40 -0700

    fixing lint

--- backend/apps/booking/serializers.py
+++ backend/apps/booking/serializers.py
@@ -2,17 +2,16 @@
     Serializer: booking app
 '''
 from rest_framework import serializers
-
-from apps.users.models import User
-from .models import BookingItems
-from apps.parkingspace.models import ParkingSpace
-
-from apps.users.serializers import UserSerializer
 from django.shortcuts import get_object_or_404
+from apps.users.models import User
+from apps.users.serializers import UserSerializer
+from apps.parkingspace.models import ParkingSpace
+from .models import BookingItems
 
 class BookingSerializer(serializers.ModelSerializer):
     tpk_booking_user = UserSerializer(required=False)
-    tpk_parkingspace = serializers.PrimaryKeyRelatedField(many=False, read_only=False, queryset=ParkingSpace.objects.all())
+    tpk_parkingspace = serializers.PrimaryKeyRelatedField(many=False, read_only=False, 
+                                                        queryset=ParkingSpace.objects.all())
     class Meta:
         model = BookingItems
         fields = '__all__'
@@ -29,8 +28,11 @@ class BookingSerializer(serializers.ModelSerializer):
         return new_booking
 
     def update(self, instance, validated_data):
-        instance.tpk_book_start_datetime = validated_data.get('tpk_book_start_datetime', instance.tpk_book_start_datetime)
-        instance.tpk_book_end_datetime = validated_data.get('tpk_book_end_datetime', instance.tpk_book_end_datetime)
+        instance.tpk_book_start_datetime = validated_data.get('tpk_book_start_datetime', 
+                                                            instance.tpk_book_start_datetime)
+        instance.tpk_book_end_datetime = validated_data.get('tpk_book_end_datetime', 
+                                                            instance.tpk_book_end_datetime)
         #instance.tpk_hourly_rate = validated_data.get('tpk_hourly_rate', instance.tpk_hourly_rate)
-        instance.tpk_book_cancelled = validated_data.get('tpk_book_cancelled', instance.tpk_book_cancelled)
+        instance.tpk_book_cancelled = validated_data.get('tpk_book_cancelled', 
+                                                            instance.tpk_book_cancelled)
         return instance
