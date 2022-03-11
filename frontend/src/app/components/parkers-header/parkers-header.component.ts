03267a9cc7253811857311e9fcdae48521614c32
digu35@gmail.com <sbbal@uwaterloo.ca>
2022-03-11 03:33:11 -0500

    PD-109 #time 2d #comment parking page design

---
+++ frontend/src/app/components/parkers-header/parkers-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parkers-header',
+  templateUrl: './parkers-header.component.html',
+  styleUrls: ['./parkers-header.component.scss']
+})
+export class ParkersHeaderComponent {
+
+  constructor() { }
+
+}
