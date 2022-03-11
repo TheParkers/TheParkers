1d36e6056596649cf9d977ca1bc6cd3d3e2512e0
digu35@gmail.com <sbbal@uwaterloo.ca>
2022-03-11 03:32:44 -0500

    Adding provider in the test case

--- frontend/src/app/services/http-interceptor/http-interceptor.service.spec.ts
+++ frontend/src/app/services/http-interceptor/http-interceptor.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { PreloaderComponent } from 'src/app/components/pre-loader/pre-loader.component';
 import { HttpInterceptorService } from './http-interceptor.service';
 
 describe('HttpInterceptorService', () => {
   let service: HttpInterceptorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        PreloaderComponent
+    ]});
     service = TestBed.inject(HttpInterceptorService);
   });
 
