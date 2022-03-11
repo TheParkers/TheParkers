1f270f6818e1f3e4106d3e88d0165f87d038e73a
digu35@gmail.com <sbbal@uwaterloo.ca>
2022-03-11 03:33:47 -0500

    PD-97 #time 3d #comment Enhance the parking API to include remaining fields. (#91)

--- backend/apps/parkingspace/serializers.py
+++ backend/apps/parkingspace/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 
 from apps.maps.models import ParkerMap
 from apps.users.models import User
-from .models import ParkingSpace, ParkingSpaceFeatures
+from .models import ParkingSpace, ParkingSpaceFeatures, ParkingSpaceImages
 
 from apps.maps.serializers import MapSerializer
 from apps.users.serializers import UserSerializer
@@ -16,10 +16,17 @@ class ParkingSpaceFeaturesSerializer(serializers.ModelSerializer):
         model = ParkingSpaceFeatures
         fields = '__all__'
 
+class ParkingSpaceImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ParkingSpaceImages
+        fields = "__all__"
+
 class ParkingSpaceSerializer(serializers.ModelSerializer):
     tpk_parking_features = ParkingSpaceFeaturesSerializer()
     tpk_ps_location = MapSerializer()
     tpk_user = UserSerializer(required=False)
+    tpk_parkingspace_images = ParkingSpaceImageSerializer(required=False, many=True, read_only=False)
+
     class Meta:
         model = ParkingSpace
         fields = '__all__'
@@ -30,16 +37,24 @@ class ParkingSpaceSerializer(serializers.ModelSerializer):
         response['tpk_parking_features'] = ParkingSpaceFeaturesSerializer(instance.tpk_parking_features).data
         response['tpk_ps_location'] = MapSerializer(instance.tpk_ps_location).data
         response['tpk_user'] = UserSerializer(instance.tpk_user).data['tpk_firebaseid']
+        response['tpk_parkingspace_images'] = ParkingSpaceImageSerializer(instance.tpk_parkingspace_images, many=True).data
         return response
 
     def create(self, validated_data):
         parkingspace_location_data = validated_data.pop('tpk_ps_location')
         parkingspace_features_data = validated_data.pop('tpk_parking_features')
         parkingspace_user_data = validated_data.pop('tpk_user')
+        parkingspace_image_data = validated_data.pop('tpk_parkingspace_images')
+        validated_data['tpk_ps_location'] = ParkerMap.objects.create(**parkingspace_location_data)
+        validated_data['tpk_parking_features'] = ParkingSpaceFeatures.objects.create(**parkingspace_features_data)
         if parkingspace_user_data:
             tpk_user = get_object_or_404(User, **parkingspace_user_data)
             validated_data['tpk_user'] = tpk_user
-        validated_data['tpk_ps_location'] = ParkerMap.objects.create(**parkingspace_location_data)
-        validated_data['tpk_parking_features'] = ParkingSpaceFeatures.objects.create(**parkingspace_features_data)
+
         parkingspace = ParkingSpace.objects.create(**validated_data)
+
+        parkingspace_images_list = []
+        for image in parkingspace_image_data:
+            parkingspace_images_list.append(ParkingSpaceImages.objects.create(tpk_parkingspace=parkingspace, **image))
+        parkingspace.tpk_parkingspace_images.set(parkingspace_images_list)
         return parkingspace
