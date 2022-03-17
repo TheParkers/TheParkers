04a346f6307ed11213b1edce28df4a66035963c2
subhambiswajit <digu35@gmail.com>
2022-03-17 17:59:59 +0000

    adding test for get signed in user

--- backend/apps/parkersauth/test/test_auth.py
+++ backend/apps/parkersauth/test/test_auth.py
@@ -1,5 +1,3 @@
-import base64
-from django.db import models
 from rest_framework.test import APITestCase, APIRequestFactory
 from unittest.mock import patch
 from rest_framework import status
@@ -36,9 +34,25 @@ class TestAuthModule(APITestCase):
         resp = self.client.post("/signin/", {"tpk_firebaseid": "token"}, format='json')
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
     
-    @patch('apps.users.models.User.objects.get')   
-    def testGetSignedInUser(self, mockUser):
-        auth_headers = {'HTTP_AUTHORIZATION': 'Bearer ' + base64.b64encode("{'tpk_email': 'test@email.com'}"),}
-        mockUser.return_value = {"tpk_email": "test", "tpk_name": "testname", "tpk_isdeleted": False, 'tpk_firebaseid': "testid"}
+    @patch('rest_framework_simplejwt.tokens.RefreshToken')   
+    @patch('apps.users.models.User.objects.get')
+    def testGetSignedInUser(self, mockUser, mockJWT):
+        test_user = User()
+        test_user.tpk_email = "test_email"
+        test_user.is_active = True
+        token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ3NTQyNTYzLCJpYXQiOjE2NDc1Mzg5NjMsImp0aSI6IjA4ZmY2MTgyNjNhODRlMWJiM2FhOTQzMWI3MDgyOWY0IiwidHBrX2VtYWlsIjoiZGlndTM1QGdtYWlsLmNvbSJ9.rH7L_PDVbqNqTHnn6lv_scDotru4VH6fP45IPTSNnwM"
+        auth_headers = {'HTTP_AUTHORIZATION': token}
+        mockUser.return_value = test_user 
         resp = self.client.get("/signin/user", {"tpk_firebaseid": "token"}, format='json',  **auth_headers)
-        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(resp.status_code, 200)
+    
+    @patch('jwt.decode')
+    @patch('rest_framework_simplejwt.tokens.RefreshToken')
+    def testGetSignedInUserWithInvalidToken(self, mockJWT, mockDecode):
+        test_user = User()
+        test_user.tpk_email = "test_email"
+        test_user.is_active = True
+        token = "Bearer eyJ0eXAiOJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ3NTQyNTYzLCJpYXQiOjE2NDc1Mzg5NjMsImp0aSI6IjA4ZmY2MTgyNjNhODRlMWJiM2FhOTQzMWI3MDgyOWY0IiwidHBrX2VtYWlsIjoiZGlndTM1QGdtYWlsLmNvbSJ9.rH7L_PDVbqNqTHnn6lv_scDotru4VH6fP45IPTSNnwM"
+        auth_headers = {}
+        resp = self.client.get("/signin/user", {"tpk_firebaseid": "token"}, format='json',  **auth_headers)
+        self.assertEqual(resp.status_code, 401)
