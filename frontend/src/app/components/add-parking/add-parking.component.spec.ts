7d57ed4df9d26859fbe0a2802c14dad043d07396
jaydeepsingh25 <jaydeepsingh_25@yahoo.com>
2022-03-22 01:52:43 -0400

    add parking component

--- frontend/src/app/components/add-parking/add-parking.component.spec.ts
+++ frontend/src/app/components/add-parking/add-parking.component.spec.ts
@@ -1,16 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { UserDetails } from 'src/app/models/responses/user';
+import { AuthService } from 'src/app/services';
+import { ParkingLocation} from 'src/app/models/parking/parking.model';
 
 import { AddParkingComponent } from './add-parking.component';
+import PlaceResult = google.maps.places.PlaceResult;
 
 describe('AddParkingComponent', () => {
   let component: AddParkingComponent;
   let fixture: ComponentFixture<AddParkingComponent>;
-
+  let authService: any;
   beforeEach(async () => {
+    let authUser: UserDetails = {
+      user:{
+          tpk_email: "testemail",
+          tpk_name: "test name", 
+          tpk_photoUrl: "test photo",
+          tpk_firebaseid: "testId"
+        }
+      }
     await TestBed.configureTestingModule({
-      declarations: [ AddParkingComponent ]
+      imports: [HttpClientModule],
+      declarations: [ AddParkingComponent ],
+      providers: [{ provide: AuthService, useValue: jasmine.createSpyObj(['registerUserToParker', 'loginUserToParker', 'getSignedInUser', 'getSignedInUser'])}],
+
     })
     .compileComponents();
+    authService = TestBed.inject(AuthService)
+    authService.getSignedInUser.and.returnValue(of(authUser))
+
   });
 
   beforeEach(() => {
@@ -22,4 +42,86 @@ describe('AddParkingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should not submit invalid form", ()=>{
+    component.addParkingForm.patchValue({
+      name: "test",
+      address: "dummy address",
+      startDate: "",
+      endDate: "",
+      description: "",
+      area: "",
+      capacity: ""
+    });
+    expect(component.addParkingForm.valid).toBeFalsy();
+    component.submitParking();
+    expect(component.invalidForm).toBeTrue();
+  });
+
+  it("should submit valid form", ()=>{
+    component.addParkingForm.patchValue({
+      name: "test",
+      address: "dummy address",
+      startDate: new Date(),
+      endDate: new Date(),
+      description: "this is test description",
+      area: "100",
+      capacity: 3,
+      price: 10
+    });
+    expect(component.addParkingForm.valid).toBeTruthy();
+    component.submitParking();
+    expect(component.invalidForm).toBeFalse();
+  });
+
+  it("Parking Location object constructed when address is filled in autocomplete", ()=>{
+    let googleAdress : PlaceResult = {
+      formatted_address: "200 University Ave W, Waterloo, ON N2L 3G1, Canada",
+      address_components: [
+        {
+          "long_name": "Waterloo",
+          "short_name": "Waterloo",
+          "types": [
+            "locality",
+            "political"
+          ]
+        },
+        {
+          "long_name": "Canada",
+          "short_name": "Canada",
+          "types": [
+            "country",
+            "political"
+          ]
+        },
+        {
+          "long_name": "N2L 3G1",
+          "short_name": "N2L 3G1",
+          "types": [
+            "postal_code"
+          ]
+        }
+      ],
+      name: 'University of Waterloo'
+    }
+    component.onAutocompleteSelected(googleAdress);
+    component.latitude = 0;
+    component.longitude = 0;
+    let resultAddress: ParkingLocation= {
+      type : "Feature",
+      geometry: {
+        type : "Point",
+        coordinates: [0,0] 
+      },
+      
+      properties: {
+        address: "200 University Ave W, Waterloo, ON N2L 3G1, Canada",
+        city: "Waterloo",
+        country: "Canada",
+        area_code: "N2L 3V5"
+      }
+    };
+    expect(component.addressDetails).toEqual(resultAddress);
+  });
+
 });
