84b5dc9961d7de5ed92cce6db7337cb1204fafe6
akshay sharma <akshay.k.sharma986@gmail.com>
2022-03-23 03:53:33 -0400

    Adding model for get booking response

---
+++ frontend/src/app/models/booking/booking.model.ts
@@ -0,0 +1,51 @@
+export const bookings= 
+{   
+    id: 1,
+    tpk_booking_user: 
+    {
+        tpk_firebaseid: "0Bf8M5rkxrREjYK0XGnDdO3tw1P2"
+    },
+    tpk_parkingspace: 
+    {
+        id: 1,
+        tpk_parking_features: 
+        {
+            id: 1, 
+            tpk_has_car_charging: true, 
+            tpk_has_car_wash: false, 
+            tpk_has_indoor_parking: false
+        },
+        tpk_ps_location: 
+        {
+            id: 1, 
+            type: "Feature", 
+            geometry:
+            {
+                type: "Point", 
+                coordinates: [43.4762, -80.54525]
+            },
+            properties: 
+            {
+                address: "address", 
+                city: "Waterloo", 
+                country: "Canada", 
+                area_code: "N2L3G1"
+            }
+        },
+        tpk_user: "0Bf8M5rkxrREjYK0XGnDdO3tw1P2",
+        tpk_parkingspace_images: [],
+        tpk_rating: 1,
+        tpk_description: "",
+        tpk_access_information: "NA",
+        tpk_price_per_hour: 2.0,
+        tpk_parking_area: 100.0,
+        tpk_has_features: true,
+        tpk_vehicle_capacity: 1,
+        tpk_created_on: "2022-03-23T03:08:01.724334Z",
+        tpk_last_booked: "2022-03-23T03:08:01.723719Z",
+        tpk_is_booked: false
+    },
+    tpk_book_start_datetime: "2022-03-23T04:08:01.794853Z",
+    tpk_book_end_datetime: "2022-03-23T05:08:01.794872Z",
+    tpk_book_cancelled: false
+}
