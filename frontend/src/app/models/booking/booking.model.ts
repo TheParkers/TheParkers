1b448395e65f7f8f278c8b6123b1f42a8ea6dfd2
akshay sharma <akshay.k.sharma986@gmail.com>
2022-03-23 03:59:33 -0400

    exporting booking response interface

--- frontend/src/app/models/booking/booking.model.ts
+++ frontend/src/app/models/booking/booking.model.ts
@@ -1,51 +1,51 @@
-export const bookings= 
+export interface bookings
 {   
-    id: 1,
+    id?: number;
     tpk_booking_user: 
     {
-        tpk_firebaseid: "0Bf8M5rkxrREjYK0XGnDdO3tw1P2"
+        tpk_firebaseid: string;
     },
     tpk_parkingspace: 
     {
-        id: 1,
+        id?: number,
         tpk_parking_features: 
         {
-            id: 1, 
-            tpk_has_car_charging: true, 
-            tpk_has_car_wash: false, 
-            tpk_has_indoor_parking: false
+            id?: number;
+            tpk_has_car_charging: true; 
+            tpk_has_car_wash: false;
+            tpk_has_indoor_parking: false;
         },
         tpk_ps_location: 
         {
-            id: 1, 
-            type: "Feature", 
+            id?: number;
+            type: string; 
             geometry:
             {
-                type: "Point", 
-                coordinates: [43.4762, -80.54525]
+                type: string;
+                coordinates: number;
             },
             properties: 
             {
-                address: "address", 
-                city: "Waterloo", 
-                country: "Canada", 
-                area_code: "N2L3G1"
+                address: string;
+                city: string;
+                country: string;
+                area_code: string;
             }
         },
-        tpk_user: "0Bf8M5rkxrREjYK0XGnDdO3tw1P2",
+        tpk_user: string;
         tpk_parkingspace_images: [],
-        tpk_rating: 1,
-        tpk_description: "",
-        tpk_access_information: "NA",
-        tpk_price_per_hour: 2.0,
-        tpk_parking_area: 100.0,
-        tpk_has_features: true,
-        tpk_vehicle_capacity: 1,
-        tpk_created_on: "2022-03-23T03:08:01.724334Z",
-        tpk_last_booked: "2022-03-23T03:08:01.723719Z",
-        tpk_is_booked: false
+        tpk_rating: string;
+        tpk_description: string;
+        tpk_access_information: string;
+        tpk_price_per_hour: number;
+        tpk_parking_area: number;
+        tpk_has_features: boolean;
+        tpk_vehicle_capacity: number;
+        tpk_created_on: Date;
+        tpk_last_booked: Date;
+        tpk_is_booked: boolean;
     },
-    tpk_book_start_datetime: "2022-03-23T04:08:01.794853Z",
-    tpk_book_end_datetime: "2022-03-23T05:08:01.794872Z",
-    tpk_book_cancelled: false
+    tpk_book_start_datetime: Date;
+    tpk_book_end_datetime: Date;
+    tpk_book_cancelled: boolean;
 }
