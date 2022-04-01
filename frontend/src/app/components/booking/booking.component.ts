59570f98a35b105e43f4a2c4ba47451902378057
akshay sharma <akshay.k.sharma986@gmail.com>
2022-04-01 18:38:27 -0400

    Create Bookings API Integration

--- frontend/src/app/components/booking/booking.component.ts
+++ frontend/src/app/components/booking/booking.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BookingService } from 'src/app/services';
 import { Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { UserDetails } from 'src/app/models/responses/user';
+import { AuthService} from 'src/app/services';
 
 @Component({
   selector: 'app-booking',
@@ -17,10 +19,18 @@ export class BookingComponent
   tpk_book_start_datetime: any;
   @Input() 
   tpk_book_end_datetime: any;
+  public signedInUser:any;
 
 
-  constructor( private modalCtr: ModalController, public bookings: BookingService) {
-
+  constructor( private modalCtr: ModalController, public bookings: BookingService, private authService: AuthService) 
+  {
+    this.authService.getSignedInUser().subscribe
+    (
+      (data: UserDetails) => 
+      {
+        this.signedInUser = data.user.tpk_firebaseid;
+      }
+    );
   }
   
 
@@ -34,7 +44,8 @@ export class BookingComponent
     (
       this.parkingSpace.id,
       this.tpk_book_start_datetime,
-      this.tpk_book_end_datetime
+      this.tpk_book_end_datetime,
+      this.signedInUser
     )
     this.modalCtr.dismiss();
   }
