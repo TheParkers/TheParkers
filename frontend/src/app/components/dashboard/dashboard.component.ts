079476f3c66a1bb04b6cd85a714bccd0d31b3424
digu35@gmail.com <your_email@abc.com>
2022-04-04 14:38:54 -0400

    adding parking map to dashboard

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -11,6 +11,11 @@ import { environment } from 'src/environments/environment.dev';
 })
 export class DashboardComponent {
   dashboard_user: any;
+  public zoom: number;
+  public latitude: number;
+  public longitude: number;
+  public rating: number = 1;
+
   constructor(private actionSheetCtrl: ActionSheetController, 
               private firebaseService: FirebaseService,
               private router: Router
@@ -26,6 +31,10 @@ export class DashboardComponent {
                     this.router.navigate(['/home'])
                   }
                 })
+
+                this.zoom = 10;
+                this.latitude = 52.520008;
+                this.longitude = 13.404954;
               }
 
    /* istanbul ignore next */
@@ -41,6 +50,62 @@ export class DashboardComponent {
 
   public navigateToAddParking(){
     this.router.navigateByUrl('/addParking');
+  }
+
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
+
+  onAutocompleteSelected(result: PlaceResult) {
+    console.log('onAutocompleteSelected: ', result);
+    let country :string = "";
+    let postal_code : string = "";
+    let city : string = "";
+    let enteredResult = result.address_components;
+    if (enteredResult){
+      this.rating = Math.floor((result.rating) || (Math.random() * (5 - 1) + 1));
+      for (const address of enteredResult){
+        //console.log(address);
+        if (address['types'].indexOf("country") != -1){
+          country = address.long_name;
+        }
+        if (address['types'].indexOf("postal_code") != -1){
+          postal_code = address.long_name;
+        }
+        if (address['types'].indexOf("locality") != -1){
+          city = address.long_name;
+        }
+  
+      }
+    }
+
+    this.addressDetails = {
+      type : "Feature",
+      geometry: {
+        type : "Point",
+        coordinates: [this.longitude, this.latitude] 
+      },
+      
+      properties: {
+        address: result.formatted_address || "",
+        city: city,
+        country: country,
+        area_code: postal_code
+      }
+    }
+
+  }
+
+  onLocationSelected(location: Location) {
+    console.log('onLocationSelected: ', location);
+    this.latitude = location.latitude;
+    this.longitude = location.longitude;
   }
 
 }
